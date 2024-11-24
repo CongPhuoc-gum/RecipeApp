cd2c872d87479bec618bd1d861710c24cfecfd6d
CongPhuoc-gum <phancongphuoc241204@gmail.com>
2024-11-24 22:22:28 +0700

    update

--- app/src/main/java/com/example/foodrecipeapp/Dashboard.java
+++ app/src/main/java/com/example/foodrecipeapp/Dashboard.java
@@ -1,24 +1,95 @@
 package com.example.foodrecipeapp;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.foodrecipeapp.fragment.ViewPaperAdapter;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Dashboard extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private BottomNavigationView bottomNavigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_dashboard);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        viewPager = findViewById(R.id.view_pager);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        ViewPaperAdapter adapter = new ViewPaperAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewPager.setAdapter(adapter);
+
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position) {
+                    case 0:
+                        bottomNavigationView.getMenu().findItem(R.id.navigation_home).setChecked(true);
+                        break;
+                    case 1:
+                        bottomNavigationView.getMenu().findItem(R.id.navigation_library).setChecked(true);
+                        break;
+                    case 2:
+                        bottomNavigationView.getMenu().findItem(R.id.navigation_messenger).setChecked(true);
+                        break;
+                    case 3:
+                        bottomNavigationView.getMenu().findItem(R.id.navigation_favorites).setChecked(true);
+                        break;
+                    case 4:
+                        bottomNavigationView.getMenu().findItem(R.id.navigation_profile).setChecked(true);
+                        break;
+
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.navigation_home) {
+                    viewPager.setCurrentItem(0);
+                    return true;
+                } else if (itemId == R.id.navigation_library) {
+                    viewPager.setCurrentItem(1);
+                    return true;
+                } else if (itemId == R.id.navigation_messenger) {
+                    viewPager.setCurrentItem(2);
+                    return true;
+                } else if (itemId == R.id.navigation_favorites) {
+                    viewPager.setCurrentItem(3);
+                    return true;
+                }else if(itemId == R.id.navigation_profile){
+                    viewPager.setCurrentItem(4);
+                    return true;
+                }
+                return false;
+            }
         });
     }
 }
--- app/src/main/java/com/example/foodrecipeapp/fragment/ViewPaperAdapter.java
+++ app/src/main/java/com/example/foodrecipeapp/fragment/ViewPaperAdapter.java
@@ -5,7 +5,41 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 
-public class ViewPaperAdapter {
-    public ViewPaperAdapter(@NonNull FragmentManager fm, int behavior){
-        super(fm, behavior);}
+public class ViewPaperAdapter extends FragmentStatePagerAdapter {
+
+    public ViewPaperAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    /**
+     * Return the number of views available.
+     */
+    @Override
+    public int getCount() {
+        return 5;
+    }
+
+    /**
+     * Return the Fragment associated with a specified position.
+     *
+     * @param position
+     */
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new FragmentHome();
+            case 1:
+                return new FragmentLibrary();
+            case 2:
+                return new FragmentMessage();
+            case 3:
+                return new FragmentFavorites();
+            case 4:
+                return new FragmentProfile();
+            default:
+                return new FragmentHome();
+        }
+    }
 }
